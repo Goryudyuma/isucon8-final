07c06daa3b01b524bc3e7c7e1a8b786301acd970
bgpat <bgpat@users.noreply.github.com>
2018-11-12 01:07:49 +0900

    Update logrotate

--- webapp/logrotate.sh
+++ webapp/logrotate.sh
@@ -5,5 +5,5 @@ set -uex
 cd `dirname $0`
 
 cp -f log/kataribe.log log/kataribe.log.old
-kataribe -f nginx/kataribe.toml < log/kataribe.log > log/kataribe.txt
 > log/kataribe.log
+kataribe -f nginx/kataribe.toml < log/kataribe.log.old > log/kataribe.txt
